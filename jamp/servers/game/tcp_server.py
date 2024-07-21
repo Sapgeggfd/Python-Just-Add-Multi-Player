d52e1e798cd59710cd4be0abbb7f2429621d17fd
Simon Ggfd <simon.ggfd@web.de>
2024-07-22 00:45:14 +0200

    rewrite tcp server

--- jamp/servers/game/tcp_server.py
+++ jamp/servers/game/tcp_server.py
@@ -1,72 +1,69 @@
-import logging
 import socket
-import threading
-from typing import Protocol
 
+from ...events.events import *
 from ...packets.tcp_packet import TCPPacket
-from ...utils.custom_logger import TCPServerLogger
-from .client import Client
-
-
-class GameServer(Protocol):
-    clients = list[Client]
-
-    def add_client(client: Client) -> bool: ...
-    def remove_client(client: Client) -> None: ...
+from ...utils.static_settings import TCP_HEADER_SIZE
+from .tcp_client import TCPClient
 
 
 class TCPServer:
-    logger: logging.Logger
+    """The TCP Server which is responsible for accepting new connection and handle incoming packages"""
 
-    running: bool
-    host: str
-    port: int
-
-    custom_client: Client
-
-    tcp_socket: socket.socket
-
-    def __init__(self, host: str, port: int, game_server: GameServer, *, custom_client: Client = Client) -> None:
-        self.logger = TCPServerLogger
+    def __init__(self, host: str, port: int) -> None:
+        self.host: str = host
+        self.port: int = port
 
         self.running = False
-        self.host = host
-        self.port = port
 
-        self.custom_client = custom_client
+        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.game_server = game_server
-
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def run(self) -> None:
+    def start(self) -> None:
+        """sets self.running to True and triggers the event on_tcp_server_start with no arguments"""
         self.running = True
-        with self.tcp_socket as sock:
-            sock.bind((self.host, self.port))
+        on_tcp_server_start.trigger()
+
+    def stop(self) -> None:
+        """sets self.running to False and triggers the event on_tcp_server_stop with no arguments"""
+        self.running = False
+        on_tcp_server_stop.trigger()
+
+    @on_tcp_server_start.register()
+    def _accept_connections(self) -> None:
+        """accepts new connections and triggers the event on_tcp_connect with the new Socket as argument"""
+        self.tcp_sock.bind((self.host, self.port))
+        with self.tcp_sock as sock:
             sock.listen()
-            self.logger.info(f"Started listening on [{self.host}:{self.port}]")
             while self.running:
-                client_sock, addr = sock.accept()
-                self.logger.info(f"New Connection from [{addr}]")
-                threading.Thread(target=self._client_connect, args=(client_sock, addr)).start()
+                new_sock, _ = sock.accept()
+                on_tcp_connect.trigger(new_socket=new_sock)
 
-    def _client_connect(self, client_sock, addr):
-        new_client: Client = self.custom_client(address=addr, tcp_sock=client_sock, tcp_server=self)
-        if new_client._wait_for_connect():
-            if self.game_server.add_client(new_client):
-                threading.Thread(target=Client._handle_tcp_packets).start()
-                return
-        del new_client
+    @on_tcp_connect.register(threaded=True)
+    def _handle_packet(self, new_sock: socket.socket) -> None:
+        """gets new packets that are received from the TCP Socket and triggers the event on_tcp_packet_received with the received packet as argument
 
-    def _clear_up_player(self):
-        while self.running:
-            dc_clients = []
-            for client in self.game_server.clients:
-                if client.disconnected:
-                    dc_clients.append(client)
-            for client in dc_clients:
-                self.game_server.remove_client(client)
+        Args:
+            new_sock (socket.socket): the socket from the event on_tcp_connect
+        """
+        with new_sock as sock:
+            while self.running:
+                try:
+                    payload_size: str = sock.recv(TCP_HEADER_SIZE)[:TCP_HEADER_SIZE].decode("utf-8")
+                    if payload_size.isdecimal():
+                        payload: bytes = sock.recv(int(payload_size))
+                        packet: TCPPacket = TCPPacket.load(payload=payload)
+                        on_tcp_packet_received.trigger(packet=packet)
+                except (EOFError, ConnectionResetError) as e:
+                    on_tcp_server_error.trigger(exception=e)
 
-    def broadcast(self, packet: TCPPacket):
-        for client in self.game_server.clients:
-            client.send(packet)
+    @on_tcp_connect.register()
+    def _generate_new_client(self, new_sock: socket.socket) -> None:
+        """Generate a new TCPClient"""
+        new_client = TCPClient(sock=new_sock)
+        # No trigger because its gets trigger in the init of the TCPClient
+
+    @on_tcp_client_disconnect.register()
+    def disconnect_client(self, client: TCPClient) -> None:
+        """Disconnects a tcp_socket and triggers the on_tcp_disconnect.trigger with the remote socket as argument"""
+        client_sock = client.tcp_sock
+        client_sock.close()
+        on_tcp_disconnect.trigger(remote_socket=client_sock)
