4b32566d66a5b6f7a68b6bc3103440e5778b835e
Simon Ggfd <simon.ggfd@web.de>
2024-07-23 15:04:26 +0200

    added doc string to OnUDPPacketReceived event

--- jamp/events/udp_events.py
+++ jamp/events/udp_events.py
@@ -5,6 +5,10 @@ class OnUDPServerStart(Event): ...
 
 
 class OnUDPPacketReceived(Event):
+    def register(self, func=None, *, threaded: bool = False, daemon_thread: bool = False):
+        """on trigger it parses the packet of type UDPPacket and the remote_addr of the socket as arguments"""
+        return super().register(func, threaded=threaded, daemon_thread=daemon_thread)
+
     def trigger(self, packet, remote_addr) -> None:
         self._call_listeners(packet, remote_addr)
 
